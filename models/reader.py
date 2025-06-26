f5835212c294a53b28764c8a8a3380ab2435587b
Михаил Шишов <145226164+mdshishov@users.noreply.github.com>
2025-06-26 15:14:48 +0300

    Update reader.py

--- models/reader.py
+++ models/reader.py
@@ -3,20 +3,8 @@
 from datetime import datetime
 import re
 
-# Здесь должна быть модель Reader согласно README.md
-
-from datetime import datetime
-import re
-
-
 class Reader:
     def __init__(self, name, email, phone) -> None:
-        if self._is_valid_name(name):
-            raise ValueError("Invalid email format")
-
-        if not self._is_valid_email(email):
-            raise ValueError("Invalid email format")
-
         self.id = None
         self.name = name
         self.email = email
@@ -27,37 +15,17 @@ class Reader:
         return re.match(r"[^@]+@[^@]+\.[^@]+", email) is not None
 
     def _is_valid_name(self, name) -> bool:
-        return name.strip() == ""
+        return name.strip() != ""
+
+    def _validate_data(self, name, email) -> None:
+        if self._is_valid_name(name):
+            raise ValueError("Invalid name")
+
+        if self._is_valid_email(email):
+            raise ValueError("Invalid email format")
 
     def update_info(self, name=None, email=None, phone=None) -> None:
-        if name is not None:
-            if self._is_valid_name(name):
-                raise ValueError("Invalid email format")
-            self.name = name
-        if email is not None:
-            if not self._is_valid_email(email):
-                raise ValueError("Invalid email format")
-            self.email = email
-        if phone is not None:
-            self.phone = phone
+        pass
 
     def to_dict(self) -> dict:
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email,
-            "phone": self.phone,
-            "registration_date": self.registration_date.strftime("%Y-%m-%d %H:%M:%S")
-        }
-# class Reader:
-#     def __init__(self, name, email, phone) -> None:
-#         pass
-#
-#     def _is_valid_email(self, email) -> bool:
-#         pass
-#
-#     def update_info(self, name=None, email=None, phone=None) -> None:
-#         pass
-#
-#     def to_dict(self) -> dict:
-#         pass
+        pass
